04f8c92b1c5bfdc601f9e9a2e7f939a8d9ac129e
Ismail <i180516@nu.edu.pk>
2022-07-29 23:05:24 +0500

    206. Reverse Linked List

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -1,3 +1,12 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Question
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
