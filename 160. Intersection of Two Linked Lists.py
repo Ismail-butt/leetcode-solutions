9785c0c3213abd7cccd53ee7e6212f98350a4bf0
Ismail <i180516@nu.edu.pk>
2022-07-31 16:08:46 +0500

    160. Intersection of Two Linked Lists

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -1,3 +1,15 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+# Question
+# Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+# If the two linked lists have no intersection at all, return null.
+
+# Example
+# Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+# Output: Intersected at '8'
+# Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+# From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
